7b370fe4947b8d92235d3f26ba6b5af548a27601
Kathoni <thrusherkeys@gmail.com>
2024-08-01 00:10:00 +0300

    installation of landing page

--- src/Components/Medium.jsx
+++ src/Components/Medium.jsx
@@ -1,22 +1,38 @@
 import React from 'react'
 
 function Medium() {
-  return (
-    <div>
+    return (
         <div>
-            <h1>Medium</h1>
             <ul>
-                <li>
-                    Our story
-                </li>
-                <li>
-                    Membership
-                </li>
-                <li>    </li>
+                <li ><h1>Medium</h1></li>
             </ul>
+            <div style={{
+                backgroundColor: "#f7e33a",
+                textAlign: "center",
+                justifyContent: "center",
+            }}>
+                <hr />
+                <b>Be part of a better.</b><u>Get 20% off membership for a limited time</u>
+                <hr />
+            </div>
+            <h4 style={{
+                fontSize: "80px",
+                fontWeight: "bold",
+                padding: "10",
+            }}>Human <br />stories & idea</h4>
+            <h3>
+                <p>A place to read ,write, and deepen your understanding</p>
+            </h3>
+            <button style={{
+                backgroundColor: "black",
+                color: "white",
+                border: "none",
+                borderRadius: "15px",
+                padding: "10px",
+                cursor: "pointer"
+            }}>start Reading</button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Medium
